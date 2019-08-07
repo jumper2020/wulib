7d7aa31f97e45e83cb15fcbba2c72330ef8ef991
jumper2019 <wjwnwk2019@gmail.com>
2019-08-07 17:01:47 +0800

    adjust comments in encoding/base58

--- encoding/interf.go
+++ encoding/interf.go
@@ -2,8 +2,6 @@ package main
 
 /* This file defines the interface of encoding implementation.*/
 
-//Input of "Encode" function - 'estr' is a binary sequence, return base64 result
-//Input of "Decode" function - 'dstr' is a base64 result, return binary sequence
 type EncodingInterf interface {
 	Encode(src []byte) []byte
 	Decode(src []byte) []byte
